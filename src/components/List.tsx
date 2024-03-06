160b81bcee445001d17af9303f0fb670d3bf5aec
lostmart <martinpedraza1979@gmail.com>
2024-03-06 19:57:12 +0100

    new not used variables

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,6 +1,6 @@
 import Car from "../interfaces/Car"
 
-const List = ({ name, year, available, price, imgUrl }: Car ) => {
+const List = ({ year }: Car) => {
 	// const { name, year, available, price, imgUrl } = props.car
 
 	return <div>the year is {year}</div>
