be24e5eac9d995c0e33e9b0ab86d287cb6b67dbc
lostmart <martinpedraza1979@gmail.com>
2024-03-24 20:34:53 +0100

    new footer css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Root from "./layouts/Root"
-// import App from './App.tsx'
+
 import "./index.css"
 
 // pages
