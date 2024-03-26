067a4a7bede1abc7d071cbc3aa17668c6fb1706f
lostmart <martinpedraza1979@gmail.com>
2024-03-26 19:24:26 +0100

    carousel swiper js

---
+++ src/components/CarrouselComp.tsx
@@ -0,0 +1,42 @@
+import React, { useRef, useState } from "react"
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react"
+
+// Import Swiper styles
+import "swiper/css"
+import "swiper/css/pagination"
+import "swiper/css/navigation"
+
+import carOne from "../imgs/car01.jpg"
+
+// import required modules
+import { Pagination, Navigation } from "swiper/modules"
+
+export default function App() {
+	return (
+		<>
+			<Swiper
+				pagination={{
+					type: "progressbar",
+				}}
+				navigation={true}
+				loop={true}
+				autoplay={true}
+				modules={[Pagination, Navigation]}
+				className="mySwiper"
+			>
+				<SwiperSlide>
+					<img src={carOne} alt="" style={{maxWidth:"100%"}} />
+				</SwiperSlide>
+				<SwiperSlide>Slide 2</SwiperSlide>
+				<SwiperSlide>Slide 3</SwiperSlide>
+				<SwiperSlide>Slide 4</SwiperSlide>
+				<SwiperSlide>Slide 5</SwiperSlide>
+				<SwiperSlide>Slide 6</SwiperSlide>
+				<SwiperSlide>Slide 7</SwiperSlide>
+				<SwiperSlide>Slide 8</SwiperSlide>
+				<SwiperSlide>Slide 9</SwiperSlide>
+			</Swiper>
+		</>
+	)
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,7 +1,10 @@
+import CarrouselComp from "../components/CarrouselComp"
+
 export default function HomePage() {
 	return (
 		<div>
 			<h2>this is the home page</h2>
+			<CarrouselComp />
 		</div>
 	)
 }
